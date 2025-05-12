0feede91281676ebf8b5bb0ca6cc3ae4b6d6cb16
Amarnath Batta <amarnath.b@gmail.com>
2025-05-12 16:15:27 -0500

    first commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,31 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "awsamplifyreact": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string"
+    }
+  },
+  "auth": {
+    "awsamplifyreact07adb24c": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "hosting": {
+    "S3AndCloudFront": {
+      "CloudFrontDistributionID": "string",
+      "CloudFrontDomainName": "string",
+      "CloudFrontOriginAccessIdentity": "string",
+      "CloudFrontSecureURL": "string",
+      "HostingBucketName": "string",
+      "Region": "string",
+      "S3BucketSecureURL": "string",
+      "WebsiteURL": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Amplify } from "aws-amplify";
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import { createTodo, updateTodo, deleteTodo } from "./graphql/mutations";
+import { generateClient } from "aws-amplify/api";
+import { getTodo, listTodos } from "./graphql/queries";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import awsExports from "./aws-exports";
+
+const client = generateClient();
+
+async function createTodoFn() {
+  const result = await client.graphql({
+    query: createTodo,
+    variables: {
+      input: {
+        name: "todo task1",
+        description: "description 111",
+      },
+    },
+  });
 }
+
+async function updateTodoFn() {
+  const result = await client.graphql({
+    query: updateTodo,
+    variables: {
+      input: {
+        id: "6261a7dc-e766-461e-9c50-c6c88d5f9258",
+        description: "task desc",
+      },
+    },
+  });
+}
+
+async function deleteTodoFn() {
+  const result = await client.graphql({
+    query: deleteTodo,
+    variables: {
+      input: {
+        id: "6261a7dc-e766-461e-9c50-c6c88d5f9258",
+      },
+    },
+  });
+}
+
+async function listTodoTasks() {
+  const result = await client.graphql({
+    query: listTodos,
+  });
+  console.log(result);
+}
+
+async function listOneTodoTask() {
+  const result = await client.graphql({
+    query: getTodo,
+    variables: { id: "25b83afa-7888-434f-94e7-b20b6129df12" }
+  });
+  console.log(result);
+}
+
+Amplify.configure(awsExports);
+
+const App = () => {
+  return (
+    <Authenticator>
+      {({ user, signOut }) => (
+        <main>
+          <h1>Welcome {user.username} !!!!!!!!!!</h1>
+          <button onClick={signOut}>Sign Out</button>
+          <button onClick={createTodoFn}> Create </button>
+          <button onClick={updateTodoFn}> Update </button>
+          <button onClick={deleteTodoFn}> Delete </button>
+          <button onClick={listTodoTasks}> List all tasks </button>
+          <button onClick={listOneTodoTask}> List one task </button>
+        </main>
+      )}
+    </Authenticator>
+  );
+};
 
 export default App;
